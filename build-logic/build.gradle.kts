41d5fdac12d0d0bef0bf455399804a8ae68f5146
fede-debe <federico.debenedictis33@gmail.com>
2026-01-31 23:45:34 +0100

    Refactor build.gradle.kts to use convention plugins

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -13,6 +13,8 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.21")
     implementation("org.jetbrains.kotlin:kotlin-allopen:2.2.21")
+    implementation("org.jetbrains.kotlin:kotlin-noarg:2.2.21")
+
     implementation("org.springframework.boot:spring-boot-gradle-plugin:4.0.2")
     implementation("io.spring.gradle:dependency-management-plugin:1.1.7")
 }
---
+++ build-logic/src/main/kotlin/chirp.spring-boot-service.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("chirp.kotlin-common")
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    kotlin("plugin.jpa")
+}
+
+dependencies {
+    "implementation"(libraries.findLibrary("kotlin-reflect").get())
+    "implementation"(libraries.findLibrary("kotlin-stdlib").get())
+    "implementation"(libraries.findLibrary("spring-boot-starter-web").get())
+
+    "testImplementation"(libraries.findLibrary("spring-boot-starter-test").get())
+    "testImplementation"(libraries.findLibrary("kotlin-test-junit5").get())
+    "testRuntimeOnly"(libraries.findLibrary("junit-platform-launcher").get())
+}

bfe4579fe9008688d55696285729386e099243c3
fede-debe <federico.debenedictis33@gmail.com>
2026-02-01 17:08:24 +0100

    Add UserEntity with JPA and PostgreSQL dependencies

---
+++ user/src/main/kotlin/com/project/user/infra/database/entities/UserEntity.kt
@@ -0,0 +1,42 @@
+package org.example.com.project.user.infra.database.entities
+
+import jakarta.persistence.*
+import org.example.com.project.user.domain.model.UserId
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.UpdateTimestamp
+import java.time.Instant
+
+/** we don't use data class here because in that way we can't run into
+ * any problems that could originate from equals override that JPA
+ * does not expect
+ *
+ * database indexes are extra info that is saved for a certain table that
+ * let us query certain fields faster */
+
+@Entity
+@Table(
+    name = "users",
+    schema = "user_service",
+    indexes = [
+        Index(name = "idx_users_email", columnList = "email"),
+        Index(name = "idx_users_username", columnList = "username")
+    ]
+)
+
+class UserEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    var id: UserId? = null,
+    @Column(nullable = false, unique = true)
+    var email: String,
+    @Column(nullable = false, unique = true)
+    var username: String,
+    @Column(nullable = false)
+    var hashedPassword: String,
+    @Column(nullable = false)
+    var hasVerifiedEmail: Boolean = false,
+    @CreationTimestamp
+    var createdAt: Instant = Instant.now(),
+    @UpdateTimestamp
+    var updatedAt: Instant = Instant.now(),
+)

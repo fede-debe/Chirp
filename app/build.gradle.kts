a9d2a5cabca213a111e3651c57ef09f366c38562
fede-debe <federico.debenedictis33@gmail.com>
2026-02-01 13:07:23 +0100

    Configure PostgreSQL datasource and JPA with environment profiles

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,4 +10,7 @@ dependencies {
 	implementation(projects.chat)
 	implementation(projects.notification)
 	implementation(projects.common)
+
+	implementation(libs.spring.boot.starter.data.jpa)
+	runtimeOnly(libs.postgresql)
 }

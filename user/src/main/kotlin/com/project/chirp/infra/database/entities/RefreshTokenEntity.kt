92f5802b661493ae6311336a9e5164fe9de73b65
fede-debe <federico.debenedictis33@gmail.com>
2026-02-07 23:22:09 +0100

    Add RefreshTokenEntity and RefreshTokenRepository

---
+++ user/src/main/kotlin/com/project/chirp/infra/database/entities/RefreshTokenEntity.kt
@@ -0,0 +1,41 @@
+package com.project.chirp.infra.database.entities
+
+import com.project.chirp.domain.model.UserId
+import jakarta.persistence.*
+import org.hibernate.annotations.CreationTimestamp
+import java.time.Instant
+
+/***
+ * Represents a refresh token entity in the database.
+ * @param id The unique identifier for the refresh token.
+ * @param userId The user ID associated with the refresh token.
+ * @param expiresAt The expiration time of the refresh token.
+ * @param hashedToken The hashed value of the refresh token. We are storing the hashed value to prevent token replay attacks.
+ * @param createdAt The creation time of the refresh token.
+ *
+ * indexes
+ * - idx_refresh_tokens_user_id: Index on the user_id column for faster queries.
+ * - idx_refresh_tokens_user_token: Index on the user_id and hashed_token columns for faster queries.
+ */
+@Entity
+@Table(
+    name = "refresh_tokens",
+    schema = "user_service",
+    indexes = [
+        Index(name = "idx_refresh_tokens_user_id", columnList = "user_id"),
+        Index(name = "idx_refresh_tokens_user_token", columnList = "user_id,hashed_token"),
+    ]
+)
+class RefreshTokenEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0,
+    @Column(nullable = false)
+    var userId: UserId,
+    @Column(nullable = false)
+    var expiresAt: Instant,
+    @Column(nullable = false)
+    var hashedToken: String,
+    @CreationTimestamp
+    var createdAt: Instant = Instant.now()
+)
---
+++ user/src/main/kotlin/com/project/chirp/infra/database/repositories/RefreshTokenRepository.kt
@@ -0,0 +1,18 @@
+package com.project.chirp.infra.database.repositories
+
+import com.project.chirp.domain.model.UserId
+import com.project.chirp.infra.database.entities.RefreshTokenEntity
+import org.springframework.data.jpa.repository.JpaRepository
+
+/***
+ * Repository for managing refresh tokens in the database.
+ * @see findByUserIdAndHashedToken: Finds a refresh token by user ID and hashed token.
+ * @see deleteByUserIdAndHashedToken: Deletes a refresh token by user ID and hashed token.
+ * @see deleteByUserId: Deletes all refresh tokens for a given user ID. This can happen when
+ * a user logs out or their session expires or the password is changed.
+ */
+interface RefreshTokenRepository : JpaRepository<RefreshTokenEntity, Long> {
+    fun findByUserIdAndHashedToken(userId: UserId, hashedToken: String): RefreshTokenEntity?
+    fun deleteByUserIdAndHashedToken(userId: UserId, hashedToken: String)
+    fun deleteByUserId(userId: UserId)
+}
